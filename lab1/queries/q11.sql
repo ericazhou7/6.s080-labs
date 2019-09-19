be1d70291b2dfb1e0707dbce331c5078fb53d634
Erica Zhou <ezhou@mit.edu>
2019-09-18 23:56:47 -0400

    fix

--- lab1/queries/q11.sql
+++ lab1/queries/q11.sql
@@ -1,2 +1,23 @@
---replace this with your query
-SELECT 1;
+-- Question: Of the 2016 Senate candidates, which had the n# of individual
+-- contributions? List top 5 with cand_name and #. Use indiv_contrib and candidate tables.
+
+SELECT
+  CAND_NAME,
+  COUNT(*) AS TOTAL_IND_CONTRIBS
+
+FROM
+  INDIV_CONTRIB,
+  CANDIDATE
+
+WHERE
+  CAND_OFFICE = 'S'
+  AND CAND_STATUS IN ('C', 'N')
+  AND CAND_ELECTION_YR = 2016
+  AND CAND_PCC = CMTE_ID
+  AND ENTITY_TP = "IND"
+
+GROUP BY
+  CAND_NAME
+
+ORDER BY
+  TOTAL_IND_CONTRIBS DESC;
