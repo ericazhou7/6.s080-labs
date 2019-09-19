ffd02165ad6ee503e58570551c682a707205f1cc
Erica Zhou <ezhou@mit.edu>
2019-09-19 11:27:41 -0400

    move q11 to folder

--- lab1/queries/q11.sql
+++ lab1/queries/q11.sql
@@ -1,23 +1,22 @@
--- Question: Of the 2016 Senate candidates, which had the n# of individual
--- contributions? List top 5 with cand_name and #. Use indiv_contrib and candidate tables.
+-- Question: Of the 2016 Presidential candidates with the most donors, who received the highest contribution
+-- per donor? List top 10 with cand_name and contribution per donor. Use indiv_contrib and candidate tables.
 
 SELECT
   CAND_NAME,
-  COUNT(*) AS TOTAL_IND_CONTRIBS
-
+  SUM(TRANSACTION_AMT) AS TOTAL_IND_CONTRIBUTIONS,
+  COUNT(*) AS TOTAL_IND_CONTRIBUTORS,
+  SUM(TRANSACTION_AMT)/COUNT(*) AS CONTRIBUTION_PER_DONOR
 FROM
   INDIV_CONTRIB,
   CANDIDATE
-
 WHERE
-  CAND_OFFICE = 'S'
+  CAND_OFFICE = 'P'
   AND CAND_STATUS IN ('C', 'N')
   AND CAND_ELECTION_YR = 2016
   AND CAND_PCC = CMTE_ID
   AND ENTITY_TP = "IND"
-
 GROUP BY
   CAND_NAME
-
 ORDER BY
-  TOTAL_IND_CONTRIBS DESC;
+  TOTAL_IND_CONTRIBUTORS DESC
+LIMIT 10;
