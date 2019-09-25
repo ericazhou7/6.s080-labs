92808776897db87204b0b2c60633be6ba206315a
Erica Zhou <ezhou@mit.edu>
2019-09-25 18:21:22 -0400

    finish pandas queries and add data

--- lab2/music.py
+++ lab2/music.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 def Q7():
-    df = pd.read_csv("data/wmbr-clean.txt", delimiter = "|")
+    df = pd.read_csv("data/wmbr-cleaner.txt", delimiter = "|")
     df = df[['Artist','Song','Album']]
     live_songs = df[df['Song'].str.contains('live',case=False) | df['Album'].str.contains('live',case=False)]
     return sorted(live_songs['Artist'].unique(),key=lambda v: v.upper())
 
 def Q8():
-    df = pd.read_csv("data/wmbr-clean.txt", delimiter = "|")
+    df = pd.read_csv("data/wmbr-cleaner.txt", delimiter = "|")
     stranger_things = df[df['Album'].str.contains('stranger things',case=False)]
     return stranger_things[['DJ','Song']].groupby('DJ').count().sort_values('Song',ascending=False)
 
 def Q9():
-    df = pd.read_csv("data/wmbr-clean2.txt", delimiter = "|")
+    df = pd.read_csv("data/wmbr-cleaner.txt", delimiter = "|")
     df['Year'] = df['Date'].str[-4:]
     df = df[df['Year'].isin(['2017','2018','2019'])]
     df['Billie'] = df['Artist'].str.contains('billie eilish',case=False)
@@ -24,14 +24,25 @@ def Q10():
     lizzo = pd.read_json('data/lizzo_appearances.json')
     lizzo_years = lizzo[lizzo['Title'].str.contains('show',case=False)]['Year'].unique()
 
-    df = pd.read_csv('data/wmbr-clean2.txt', delimiter = '|')
+    df = pd.read_csv('data/wmbr-cleaner.txt', delimiter = '|')
     df['Year'] = df['Date'].str[-4:]
     df = df[df['Year'].isin(lizzo_years)]
     lizzo_songs = df[df['Artist'].str.contains('lizzo',case=False) | df['Song'].str.contains('lizzo',case=False)]
     return lizzo_songs['Song'].value_counts().reset_index().sort_values(['Song','index'],ascending=[False,True])
 
-# def Q11():
+def Q11():
+    wmbr = pd.read_csv('data/wmbr-cleaner.txt',delimiter='|')
+    wmbr['Artist'] = wmbr['Artist'].str.split(',')
+    wmbr['Artist'] = wmbr['Artist'].apply(lambda x: [val.strip() for val in x])
+    wmbr['Artist'] = wmbr['Artist'].apply(lambda x: set(x))
+    wmbr_artists = set.union(*wmbr['Artist'])
 
-queries = [Q10]
+    top2018 = pd.read_csv('data/top2018.csv')
+    top2018_wmbr = top2018[top2018['artists'].isin(wmbr_artists)]
+    return top2018_wmbr.sort_values('danceability',ascending=False)[['name','artists','danceability']]
+
+queries = [Q7,Q8,Q9,Q10,Q11]
+
 for query in queries:
+    print('\n' + query.__name__)
     print(query())
